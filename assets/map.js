df31dbe19f883101525e1c2319a82bc364b1abd7
EliotChignell <chignelleliot@gmail.com>
2024-06-01 23:11:33 +1200

    Fixed a bug with Washington D.C.

--- assets/map.js
+++ assets/map.js
@@ -1,10 +1,10 @@
-const map = require('./map.json');
+const map = require('./all_data.json');
 const fs = require('fs');
 
 const a = {};
 
 for (const c of map) {
-    const n = c.city_ascii.toLowerCase();
+    const n = c.city_ascii.toLowerCase().replace(',', '');
     if (!a[n]) a[n] = [];
 
     delete c.lat;
@@ -13,11 +13,11 @@ for (const c of map) {
     delete c.pop;
     delete c.city;
 
-    c.city = c.city_ascii;
+    c.city = c.city_ascii.replace(',', '');
 
     delete c.city_ascii;
 
     a[n].push(c);
 }
 
-fs.writeFileSync('map.json', JSON.stringify(a));
+fs.writeFileSync('scripts/cities.js', 'const cities = ' + JSON.stringify(a));
